26aa1c73798681892304d3d24f3f0294888a2e4a
leimingze <2454316259@qq.com>
2026-02-24 22:33:48 +0800

    1.6.2

--- src/ragmcp/factory/__init__.py
+++ src/ragmcp/factory/__init__.py
@@ -1,5 +1,8 @@
 """Factory module for creating component instances."""
 
+from ragmcp.factory.embedding_factory import EmbeddingFactory
 from ragmcp.factory.llm_factory import LLMFactory
+from ragmcp.factory.vector_store_factory import VectorStoreFactory
+from ragmcp.factory.vision_llm_factory import VisionLLMFactory
 
-__all__ = ["LLMFactory"]
+__all__ = ["LLMFactory", "EmbeddingFactory", "VisionLLMFactory", "VectorStoreFactory"]
